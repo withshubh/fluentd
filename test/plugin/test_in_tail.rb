a82336adc473c0a2a4f7f919a6eaebb2a697fde2
Hiroshi Hatake <hatake@clear-code.com>
2021-01-07 13:31:36 +0900

    Fix a typo
    Signed-off-by: Hiroshi Hatake <hatake@clear-code.com>

--- test/plugin/test_in_tail.rb
+++ test/plugin/test_in_tail.rb
@@ -312,7 +312,7 @@ class TailInputTest < Test::Unit::TestCase
       assert num_events <= d.emit_count
     end
 
-    sub_test_case "log throttoling per file" do
+    sub_test_case "log throttling per file" do
       teardown do
         FileUtils.rm_f("#{TMP_DIR}/tail.txt")
       end
